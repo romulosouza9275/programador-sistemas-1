19bad12a9a26a7ee1ec0a9e4fcc0501c62ede321
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2018-10-23 15:52:12 -0300

    Add files via upload

---
+++ aula04/001_OlaMundo/src/controller/OlaMundo.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class OlaMundo {
+
+	/*
+	 *  Este é o seu primeiro programa em JAVA
+	 */
+
+	public static void main(String[] args)	{
+		System.out.println("Olás Mundo!");
+		System.out.println(10);
+	}	
+
+	
+}
