45f3e7e6da202b692ba7d05c35ab36d8bc04748f
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2018-10-23 15:53:16 -0300

    Add files via upload

---
+++ aula06/002_ExemploGUI/src/controller/Exemplo.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class Exemplo {
+	public static void main(String[] args) {
+		//javax.swing.JOptionPane.showMessageDialog(null,	"SENAI","Exemplo GUI", 1);
+		javax.swing.JOptionPane.showConfirmDialog(null, "Quer aumento de salário?", "Salário",
+				javax.swing.JOptionPane.YES_NO_OPTION);
+		}
+}
