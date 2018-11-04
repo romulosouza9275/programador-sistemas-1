c6ffb1bd60c1bbf1f3a3d752f3fd5f741df48758
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2018-11-04 16:30:23 -0200

    Add files via upload

---
+++ aula17/017_VerdadeiroFalso/src/controller/Teste.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class Teste {
+	public static void main(String[] args) {
+		boolean b = false;
+		boolean a = true;
+		boolean c = b && !b;
+		if(c)
+		{
+			System.out.println(c);
+		}
+		/*if ( a != b ) {
+			System.out.println("Diferentes");
+		}
+		else
+		{
+			System.out.println("Iguais");
+		}*/
+		
+	}
+}
